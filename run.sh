d1a6a6764e8ee41674da0d04f028275ef420a224
Kpacha <kpacha666@gmail.com>
2015-10-04 22:41:04 +0200

    set all params as env_var

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-python ./mesos_ec2.py -u "http://${MESOS_HOST}:${MESOS_PORT}" -c "${MIN_CPUS},${MAX_CPUS}" -r "${EC2_REGION}" -a  "${EC2_ASG}" -l ${AUTOSCALER_LOG_LEVEL}
+python ./mesos_ec2.py -u "http://${MESOS_HOST}:${MESOS_PORT}" \
+    -c "${MIN_CPUS},${MAX_CPUS}" -r "${EC2_REGION}" \
+    -a "${EC2_ASG}" -l "${AUTOSCALER_LOG_LEVEL}" \
+    -d "${MIN_DISK},${MAX_DISK}" -m "${MIN_MEM},${MAX_MEM}"
